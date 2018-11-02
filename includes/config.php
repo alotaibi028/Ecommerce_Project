4d857a1e562a0e279f4019252133b27763dc62c0
alotaibi028 <42802405+alotaibi028@users.noreply.github.com>
2018-11-02 13:52:30 -0700

    Update includes/config.php

--- includes/config.php
+++ includes/config.php
@@ -3,8 +3,8 @@
 
 $db_host = "localhost";
 $db_username = "root";
-$db_password = "";
-$db_name = "com";
+$db_password = "7606MA";
+$db_name = "db";
 
 $con = mysqli_connect($db_host,$db_username,$db_password,$db_name);
 
