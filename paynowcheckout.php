dbc7c7cdd839a95f0b96383143d74337953b3539
alotaibi028 <42802405+alotaibi028@users.noreply.github.com>
2018-11-08 01:14:53 -0800

    edit

--- paynowcheckout.php
+++ paynowcheckout.php
@@ -86,7 +86,88 @@ if(isset($_REQUEST['stripeToken'])){
                 echo     mysqli_error($con);
                 }
 
-                
+                // email
+// Create the email and send the message
+echo $to =strip_tags($_SESSION['u_email']); // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Congratulation Your order has been placed";
+ // PREPARE THE BODY OF THE MESSAGE
+ // Customer Email
+  $total_quantitys = 0;
+            $total_prices = 0;
+		$email_body = '<html><body>';
+			$email_body .= '<h2><center>'.$lang['order_summary'].'</center></h2><br />';
+			$email_body .= '<div style="background-color: gainsboro"><table class="tbl-cart" cellpadding="10" style="margin-left: 10%" cellspacing="1"><tbody><tr>';
+			$email_body .= '<th style="text-align:left;">'.$lang['name'].'</th><th style="text-align:right;" width="5%">'. $lang['quantity'].'</th><th style="text-align:right;" width="25%">'. $lang['unit_price'].'</th><th style="text-align:right;" width="25%">'. $lang['price'].'</th> </tr>';
+				foreach ($_SESSION["cart_item"] as $item){
+                    $item_price = $item["quantity"]*$item["price"];
+                    $sql = "select * from products where id = ".$item['id'];
+                    $result = mysqli_query($con, $sql);
+                    $row = $result ->fetch_array();
+                    $item_price = $item["quantity"]*$item["price"];
+					if($_SESSION['lang'] == 'arabic'){ $pname = $row['name_ar']; }else{ $pname =  $row['name'];};
+				
+			$email_body .= '<tr><td>'.$pname.'</td><td style="text-align:right;">'. $item["quantity"].'</td><td  style="text-align:right;">'. "SR ".$item["price"] .'</td><td  style="text-align:right;">'. "SR". number_format($item_price,2).'</td></tr>';
+				}
+				$total_quantitys += $item["quantity"];
+                    $total_prices += ($item["price"]*$item["quantity"]);
+			$email_body .= ' <tr><td colspan="1" align="right">'. $lang['total'].' </td><td align="right">'. $total_quantity.' </td><td align="right" colspan="2"><strong>'. "SR ".number_format($total_price, 2).' </strong></td><td></td></tr></tbody></table>';
+			$email_body .= "</body></html>";
+			
+			$headers = "From: otaibimk1428@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+			$headers .= "Reply-To: otaibimk1428@gmail.com\r\n";
+			$headers .= "MIME-Version: 1.0\r\n";
+			$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+			$headers .= "X-Mailer: PHP/".phpversion();   
+			echo $email_body;
+mail($to,$email_subject,$email_body,$headers);
+exit;
+
+// Vendor Email
+$vendorArray = array();
+
+			foreach ($_SESSION["cart_item"] as $item){
+				
+				  $total_quantitys = 0;
+            $total_prices = 0;
+                    $item_price = $item["quantity"]*$item["price"];
+                    $sql = "select * from products where id = ".$item['id'];
+                    $result = mysqli_query($con, $sql);
+                    $row = $result ->fetch_array();
+                    $item_price = $item["quantity"]*$item["price"];
+					
+					$vendorID = $row['added_by'];
+					
+					$sqlVendor = " SELECT * FROM users WHERE id=" . $vendorID;
+					$resultVendor = mysqli_query($con, $sqlVendor);
+					$rowVendor = $resultVendor->fetch_array();
+					
+					$vendorEmail = $rowVendor['email'];
+					
+					if($_SESSION['lang'] == 'arabic'){ $pname = $rowVendor['name_ar']; }else{ $pname =  $rowVendor['name'];};
+					$total_quantitys += $item["quantity"];
+                    $total_prices += ($item["price"]*$item["quantity"]);
+					$email_body = '<html><body>';
+			$email_body .= '<h2><center>'.$lang['order_summary'].'</center></h2><br />';
+			$email_body .= '<div style="background-color: gainsboro"><table class="tbl-cart" cellpadding="10" style="margin-left: 10%" cellspacing="1"><tbody><tr>';
+			$email_body .= '<th style="text-align:left;">'.$lang['name'].'</th><th style="text-align:right;" width="5%">'. $lang['quantity'].'</th><th style="text-align:right;" width="25%">'. $lang['unit_price'].'</th><th style="text-align:right;" width="25%">'. $lang['price'].'</th> </tr>';
+				
+					$email_body .= '<tr><td>'.$pname.'</td><td style="text-align:right;">'. $item["quantity"].'</td><td  style="text-align:right;">'. "SR ".$item["price"] .'</td><td  style="text-align:right;">'. "SR ". number_format($item_price,2).'</td></tr>';
+				$email_body .= ' <tr><td colspan="1" align="right">'. $lang['total'].' </td><td align="right">'. $total_quantity.' </td><td align="right" colspan="2"><strong>'. "SR ".number_format($total_price, 2).' </strong></td><td></td></tr></tbody></table>';
+			$email_body .= "</body></html>";
+			
+					$headers = "From: otaibimk1428@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+						$headers .= "Reply-To: otaibimk1428@gmail.com\r\n";
+						$headers .= "MIME-Version: 1.0\r\n";
+						$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+						$headers .= "X-Mailer: PHP/".phpversion();   
+						mail($vendorEmail,'You have a new Order',$email_body,$headers);
+					
+					}
+				
+			
+			
+
+                unset($_SESSION["cart_item"]);
 
                     $statusMsg = 1;
                     echo '<script type="text/javascript">';
