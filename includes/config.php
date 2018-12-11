474e7b3b607923fe45699f8615cc355727770cc7
alotaibi028 <42802405+alotaibi028@users.noreply.github.com>
2018-12-10 22:01:45 -0800

    edit

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 $db_host = "localhost";
 $db_username = "root";
 $db_password = "7606MA";
-$db_name = "db";
+$db_name = "ecommerceZ_db";
 
 $con = mysqli_connect($db_host,$db_username,$db_password,$db_name);
 
