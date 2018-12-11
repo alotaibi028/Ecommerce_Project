bce5fd61b611ffd595aeeec47e61588cfdf18691
alotaibi028 <42802405+alotaibi028@users.noreply.github.com>
2018-12-10 21:42:25 -0800

    edit

--- includes/config.php
+++ includes/config.php
@@ -3,8 +3,8 @@
 
 $db_host = "localhost";
 $db_username = "cottage_ecommerc";
-$db_password = "ecommerce@123";
-$db_name = "cottageindustryvirtualmarket_ecommerce";
+$db_password = "7606MA";
+$db_name = "db";
 
 $con = mysqli_connect($db_host,$db_username,$db_password,$db_name);
 
